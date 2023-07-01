d66ccd702b57369ac08eda00832c95ff55d52e25
Matan Levy <levymatanlevy@gmail.com>
2023-07-01 10:34:07 +0300

    A working version! still ugly

--- script.js
+++ script.js
@@ -1,97 +1,45 @@
 document.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById('pregnancy-form');
+  const calendarContainer = document.getElementById('calendar-container');
+  const downloadLink = document.getElementById('download-link');
+  const successMessage = document.getElementById('success-message');
 
-    // Check if ics library is available
-    if (typeof ics !== 'undefined') {
-        // Code to execute when the ics library is available
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    generateICal();
+  });
 
-        // Get form element and attach submit event listener
-        const form = document.getElementById('pregnancy-form');
-        form.addEventListener('submit', function(event) {
-            event.preventDefault();
+  function generateICal() {
+    const startDate = document.getElementById('start-date').value;
+    const pregnancyWeeks = parseInt(document.getElementById('pregnancy-weeks').value);
+    const pregnancyDays = parseInt(document.getElementById('pregnancy-days').value);
+    const eventDuration = parseInt(document.getElementById('event-duration').value);
 
-            // Get selected method
-            const methodSelect = document.getElementById('method');
-            const selectedMethod = methodSelect.value;
+    const cal = ics();
 
-            // Get input values based on selected method
-            let startDate;
-            let pregnancyWeeks;
-            let pregnancyDays;
+    let currentDate = new Date(startDate);
 
-            if (selectedMethod === 'last-period') {
-                const lastPeriodInput = document.getElementById('last-period');
-                startDate = new Date(lastPeriodInput.value);
-            } else if (selectedMethod === 'progress') {
-                const progressDateInput = document.getElementById('progress-date');
-                startDate = new Date(progressDateInput.value);
+    for (let week = 1; week <= pregnancyWeeks; week++) {
+      for (let day = 1; day <= pregnancyDays; day++) {
+        const endDate = new Date(currentDate.getTime() + (eventDuration - 1) * 24 * 60 * 60 * 1000);
 
-                const pregnancyWeeksInput = document.getElementById('pregnancy-weeks');
-                const pregnancyDaysInput = document.getElementById('pregnancy-days');
-                pregnancyWeeks = parseInt(pregnancyWeeksInput.value);
-                pregnancyDays = parseInt(pregnancyDaysInput.value);
-            } else if (selectedMethod === 'due-date') {
-                const dueDateInput = document.getElementById('due-date');
-                startDate = new Date(dueDateInput.value);
-            }
+        cal.addEvent(`Week ${week} - Day ${day}`, '', '', currentDate, endDate);
 
-            // Get event duration
-            const eventDurationSelect = document.getElementById('event-duration');
-            const eventDuration = parseInt(eventDurationSelect.value);
-
-            // Generate iCal events
-            const events = generatePregnancyEvents(startDate, pregnancyWeeks, pregnancyDays, eventDuration);
-
-            // Generate iCal file
-            const { error, value } = ics.createEvents(events);
-
-            if (error) {
-                console.error(error);
-                return;
-            }
-
-            // Create a blob from the iCal data
-            const blob = new Blob([value], { type: 'text/calendar' });
-
-            // Create a download link and trigger the download
-            const link = document.createElement('a');
-            link.href = URL.createObjectURL(blob);
-            link.download = 'pregnancy_schedule.ics';
-            link.click();
-
-            // Display success message
-            const successMessage = document.getElementById('success-message');
-            successMessage.style.display = 'block';
-            successMessage.textContent = 'iCal file generated successfully!';
-        });
-    } else {
-        // Code to execute when the ics library is not available
-
-        // Display error message
-        const errorMessage = document.getElementById('success-message');
-        errorMessage.style.display = 'block';
-        errorMessage.textContent = 'Unable to generate iCal file. Please try again later.';
-        console.log('ics library is NOT available');
-
-        // Fallback to ical.js
-        loadIcalLibrary();
+        currentDate.setDate(currentDate.getDate() + 1);
+      }
     }
+
+    const calendarContent = cal.download();
+    calendarContainer.innerHTML = calendarContent;
+    calendarContainer.style.display = 'block';
+
+    const filename = 'pregnancy_schedule.ics';
+    const blob = new Blob([calendarContent], { type: 'text/calendar;charset=utf-8' });
+    const downloadUrl = URL.createObjectURL(blob);
+    downloadLink.href = downloadUrl;
+    downloadLink.setAttribute('download', filename);
+    downloadLink.style.display = 'block';
+
+    successMessage.textContent = 'iCal file generated successfully.';
+  }
 });
-
-function loadIcalLibrary() {
-    const script = document.createElement('script');
-    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/ical.js/4.8.0/ical.js';
-    script.onload = function() {
-      console.log('ical.js library has been loaded');
-      // Code to execute when the ical.js library is loaded
-
-      // Rest of your code goes here...
-    };
-    script.onerror = function() {
-      console.log('Failed to load ical.js library');
-      // Display error message
-      const errorMessage = document.getElementById('success-message');
-      errorMessage.style.display = 'block';
-      errorMessage.textContent = 'Unable to generate iCal file. Please try again later.';
-    };
-    document.head.appendChild(script);
-}
