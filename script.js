81a32f1c31bfcefebf6a0163435276c0d09a81d9
Matan Levy <levymatanlevy@gmail.com>
2023-07-06 00:11:22 +0300

    Functional website!!

--- script.js
+++ script.js
@@ -1,12 +1,68 @@
 $(document).ready(function() {
-    $('#display-button').click(function() {
+    $('#input-option').change(function() {
+      const selectedOption = $(this).val();
+      $('.input-fields').hide();
+      $(`#${selectedOption}-input-fields`).show();
+      $('#display-button').prop('disabled', true); // Disable the display button
+    });
+
+    // Function to check if all required input fields are filled
+    function validateInputs() {
+      const inputOption = $('#input-option').val();
+
+      if (inputOption === 'date') {
         const date = $('#date-input').val();
         const weekOfPregnancy = parseInt($('#week-input').val());
         const dayOfPregnancy = parseInt($('#day-input').val());
-        const resolution = $('#resolution-select').val();
 
-        const startDate = calculateStartDate(date, weekOfPregnancy, dayOfPregnancy);
-        const events = calculatePregnancyEvents(startDate, resolution);
+        if (date && weekOfPregnancy && dayOfPregnancy) {
+          return true;
+        }
+      } else if (inputOption === 'last-period') {
+        const lastPeriodDate = $('#last-period-input').val();
+
+        if (lastPeriodDate) {
+          return true;
+        }
+      } else if (inputOption === 'due-date') {
+        const dueDate = $('#due-date-input').val();
+
+        if (dueDate) {
+          return true;
+        }
+      }
+
+      return false;
+    }
+
+    // Event listener for input fields
+    $('.input-fields input').on('input', function() {
+      if (validateInputs()) {
+        $('#display-button').prop('disabled', false); // Enable the display button
+      } else {
+        $('#display-button').prop('disabled', true); // Disable the display button
+      }
+    });
+
+    $('#display-button').click(function() {
+      const inputOption = $('#input-option').val();
+      const resolution = $('#resolution-select').val();
+
+      let startDate;
+      if (inputOption === 'date') {
+        const date = $('#date-input').val();
+        const weekOfPregnancy = parseInt($('#week-input').val());
+        const dayOfPregnancy = parseInt($('#day-input').val());
+        startDate = calculateStartDate(date, weekOfPregnancy, dayOfPregnancy);
+      } else if (inputOption === 'last-period') {
+        const lastPeriodDate = $('#last-period-input').val();
+        startDate = calculateStartDateFromLastPeriodDate(lastPeriodDate);
+      } else if (inputOption === 'due-date') {
+        const dueDate = $('#due-date-input').val();
+        startDate = calculateStartDateFromDueDate(dueDate);
+      }
+
+      const events = calculatePregnancyEvents(startDate, resolution);
 
         $('#calendar').fullCalendar('destroy');
         $('#calendar').fullCalendar({
@@ -19,6 +75,19 @@ $(document).ready(function() {
     });
 });
 
+/**
+ *
+ * @param {*} dueDate
+ * @returns
+ */
+function calculateStartDateFromDueDate(dueDate) {
+    return moment(dueDate).subtract(40, 'weeks');
+}
+
+function calculateStartDateFromLastPeriodDate(lastPeriodDate) {
+    return moment(lastPeriodDate).add(1, 'day');
+}
+
 function calculateStartDate(date, weekOfPregnancy, dayOfPregnancy) {
     return moment(date).subtract(weekOfPregnancy - 1, 'weeks').subtract(dayOfPregnancy - 1, 'days');
 }
@@ -27,27 +96,29 @@ function calculatePregnancyEvents(startDate, resolution) {
     const currentDate = moment();
     const events = [];
 
-    if (resolution === 'day') {
-        for (let i = 1; i <= 40; i++) {
-            const eventStart = startDate.clone().add(i - 1, 'days');
-            const eventEnd = eventStart.clone().add(1, 'days');
+    for (let i = 1; i <= 40; i++) {
+        let eventStart, eventEnd;
 
-            const event = {
-                title: `Week ${i} - Day ${eventStart.format('D')}`,
-                start: eventStart.format('YYYY-MM-DD'),
-                end: eventEnd.format('YYYY-MM-DD')
-            };
+        if (resolution === 'day') {
+            for (let day = 1; day <= 7; day++) {
+                eventStart = startDate.clone().add((i - 1) * 7 + (day - 1), 'days');
+                eventEnd = eventStart.clone().add(1, 'days');
 
-            if (currentDate.isBetween(eventStart, eventEnd, 'day', '[]')) {
-                event.color = 'red';
+                const event = {
+                    title: `Week ${i} - Day ${day}`,
+                    start: eventStart.format('YYYY-MM-DD'),
+                    end: eventEnd.format('YYYY-MM-DD')
+                };
+
+                if (currentDate.isBetween(eventStart, eventEnd, 'day', '[]')) {
+                    event.color = 'red';
+                }
+
+                events.push(event);
             }
-
-            events.push(event);
-        }
-    } else {
-        for (let i = 1; i <= 40; i++) {
-            const eventStart = startDate.clone().add((i - 1) * 7, 'days');
-            const eventEnd = eventStart.clone().add(7, 'days');
+        } else {
+            eventStart = startDate.clone().add((i - 1) * 7, 'days');
+            eventEnd = eventStart.clone().add(7, 'days');
 
             const event = {
                 title: `Week ${i}`,
